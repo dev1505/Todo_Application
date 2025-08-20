217ea8f8224cee9294d23772d8f946d8abb6f42a
dev <devshah152004@gmail.com>
2025-08-20 12:31:53 +0000

    changes in styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const { todoAppData, setTodoAppData } = useContext(GlobalContext);
   return (
     <>
-      <div className="bg-black h-screen">
+      <div className="bg-black min-h-screen">
         {
           todoAppData?.allComponents?.map((data, index) => {
             const Component = data?.component;
